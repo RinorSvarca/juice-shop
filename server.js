c764fb80c212edd97ec8e112713b13249aed74e3
Rinor <rinorsvarca@gmail.com>
2018-04-16 22:39:59 +0200

    Creating Custom Event Log file

--- server.js
+++ server.js
@@ -127,6 +127,14 @@ app.use(bodyParser.json())
 let accessLogStream = require('file-stream-rotator').getStream({filename: './access.log', frequency: 'daily', verbose: false, max_logs: '2d'})
 app.use(morgan('combined', {stream: accessLogStream}))
 
+/* HTTP request logging - Wissenschaftliches Projekt */
+let accessLogStream_p_eigenes = require('file-stream-rotator').getStream({filename: './app/access_p_eigenes.txt', frequency: 'aaaaa', verbose: false})
+app.use(morgan('p_eigenes', {stream: accessLogStream_p_eigenes}))
+
+/* HTTP request logging - Wissenschaftliches Projekt - Alles in einer Datei */
+let accessLogStream_p_combined = require('file-stream-rotator').getStream({filename: './app/access_p_combined.txt', frequency: 'aaaaa', verbose: false})
+app.use(morgan('combined', {stream: accessLogStream_p_combined}))
+
 /** Authorization **/
 /* Checks on JWT in Authorization header */
 app.use(verify.jwtChallenges())
